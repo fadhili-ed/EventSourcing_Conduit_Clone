42e06d249c313ec0e4899d335933c8d999a8eec1
Edwin Fadhili Omondi <fadhilied00@gmail.com>
2023-01-16 17:28:57 +0300

    initial commit: working model after creating and initializing eventstore db

---
+++ config/config.exs
@@ -0,0 +1,65 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :conduit,
+  ecto_repos: [Conduit.Repo]
+
+# Configures the endpoint
+config :conduit, ConduitWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: ConduitWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Conduit.PubSub,
+  live_view: [signing_salt: "AbnMjJ7+"]
+
+config :commanded,
+  event_store_adapter: Commanded.EventStore.Adapters.EventStore
+
+config :conduit, event_stores: [Conduit.EventStore]
+
+config :conduit, Conduit.App,
+  event_store: [
+    adapter: Commanded.EventStore.Adapters.EventStore,
+    event_store: Conduit.EventStore
+  ],
+  pub_sub: :local,
+  registry: :local
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :conduit, Conduit.Mailer, adapter: Swoosh.Adapters.Local
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.14.29",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
